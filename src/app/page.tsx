48600e6d33083d5918e48dc18265110622386d28
Nikolay <ya.sapunovops2.ru@list.ru>
2023-08-29 11:15:54 +0300

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { getAsteroids } from "@/utils"
 import MainPage from "./components/pages/MainPage/MainPage"
+import Head from "next/head"
 
 export default async function Home() {
     const data = await getAsteroids()
     
     return (
-        <MainPage data={data}/>
+        <>
+            <Head>
+                <meta name="format-detection" content="telephone=no"/>
+                <meta name="format-detection" content="date=no" /> 
+            </Head>
+            <MainPage data={data}/>
+        </>
     )
 }
